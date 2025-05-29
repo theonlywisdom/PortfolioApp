ec54aab84106c9761edb0d90bfb6e0823186024a
Wisdom Akwoyere Baah <WABaah@edinst.com>
2025-05-29 21:07:35 +0100

    add PortfolioAppContext

---
+++ PortfolioApp.DataAccess/PortfolioAppContext.cs
@@ -0,0 +1,16 @@
+﻿namespace PortfolioApp.DataAccess;
+
+public class PortfolioAppContext : DbContext
+{
+    public DbSet<Portfolio> Portfolios { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<CountryAdjustment> Adjustments { get; set; }
+    public DbSet<RunMetadata> Runs { get; set; }
+    public DbSet<AggregatedResult> Results { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=PortfolioApp.db");
+    }
+}
