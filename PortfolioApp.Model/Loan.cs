f324cc163684d607a17a8ebec8a0d7a538b8bd18
theonlywisdom <Wisdom.Baah@edinst.com>
2025-05-31 00:26:53 +0100

    add original amount to load model

--- PortfolioApp.Model/Loan.cs
+++ PortfolioApp.Model/Loan.cs
@@ -4,8 +4,9 @@ public class Loan
 {
     public int LoanId { get; set; }
     public int PortfolioId { get; set; }
-    public string CreditRating { get; set; }
+    public decimal OriginalLoanAmount { get; set; }
     public decimal OutstandingAmount { get; set; }
     public decimal CollateralValue { get; set; }
+    public string CreditRating { get; set; }
     public Portfolio Portfolio { get; set; }
 }
